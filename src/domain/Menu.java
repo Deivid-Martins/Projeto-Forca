2a01ed96391c8a1a9eccf248cd2f969078289f93
Deivid Martins <159496589+Deivid-Martins@users.noreply.github.com>
2024-08-29 17:08:56 -0300

    a

--- src/domain/Menu.java
+++ src/domain/Menu.java
@@ -1,5 +1,20 @@
 package domain;
 
 public class Menu {
-
+	String[] menu; // As opções presentes no menu
+	
+	
+	//Construtor do menu
+	public Menu(String[] menu){
+		this.menu = menu;
+	}
+	
+	
+	// Imprime o menu
+	public void imprimirMenu() {
+		int i; // Iterador
+		for(i = 0; i < menu.length; i++) {
+			System.out.printf("[ %d ] - %s\n", i+1, menu[i]); // Exibe uma opção do array
+		}
+	}
 }
