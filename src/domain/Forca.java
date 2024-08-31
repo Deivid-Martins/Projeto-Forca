d43596b30b9a6bb5f30c745caa1f4bf0a6b65d29
Kayke Nícolas <148629652+KaykeNicolas007@users.noreply.github.com>
2024-08-31 15:22:10 -0300

    Mudanças

--- src/domain/Forca.java
+++ src/domain/Forca.java
@@ -1,17 +1,19 @@
 // Classe que gerencia o jogo da forca em si
 package domain;
 
-import java.util.Random;
 import java.util.Scanner;
 
 public class Forca {
-	private Random random = new Random();
 	private Scanner input = new Scanner(System.in);
 	
 	public String word; // Palavra escolhida
 	public Player[] players; // Array de jogadores indefinido
+	public Dicionario dict = new Dicionario(); // Dicionário de palavras
 	
+	
+	// Construtor da forca com jogadores e palavra definidos
 	public Forca () {
+		// Criação dos jogadores
 		int numPlayers; // Quantidade de jogadores
 		
 		System.out.print("Defina a quantidade de jogadores: "); // Prompt
@@ -21,46 +23,13 @@ public class Forca {
 		for(int i = 0; i < numPlayers; i++) {
 			players[i] = new Player(); // Cria um novo jogador para cada posição do array
 		}
+		
+		
+		// Definição da palavra
+		this.word = dict.randomWord();
 	}
 
 	public String getWord() {
 		return word;
-	}
-	
-	public void setOrdem () {
-		boolean aux = false;
-		for(int i = 0; i < players.length; i ++) {
-			
-			while(aux == false) {
-				for(int j = 0; j < players.length; j ++) {
-				
-					
-					players[i].ordem = random.nextInt(players.length) + 1;
-					if (i != j) {
-						if (players[i].ordem == players[j].ordem) {
-							aux = true;
-						}
-					}
-				}
-			}
-		}
-	}
-	
-	public void changePlayerPos (Player p1, Player p2) {
-		Player aux;
-		int indexP1 = 0;
-		int indexP2 = 0;
-		for(int i = 0; i < players.length; i ++) {
-			if(p1 == players[i]) {
-				indexP1 = i;
-			}
-			if(p2 == players[i]) {
-				indexP2 = i;
-			}
-		}
-		
-		aux = players[indexP1];
-		players[indexP1] = players[indexP2];
-		players[indexP2] = aux;
 	}
 }
