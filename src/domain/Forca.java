e4f63343981a39a2875f2e9b4d3e87cdc157be2e
Deivid Martins <159496589+Deivid-Martins@users.noreply.github.com>
2024-08-31 14:49:07 -0300

    a

--- src/domain/Forca.java
+++ src/domain/Forca.java
@@ -25,55 +25,7 @@ public class Forca {
 		return word;
 	}
 	
-	// Função para definir quem vai por ultimo, realizada após a função, zero ou um, AINDA NÂO IMPLEMENTADA
-	private void pedraPapelTesoura (Player p1, Player p2) { // N se preocupe com esse erro, fará sentido
-		int e1; // Escolha do player 1
-		int e2; // Escolha do player 2
-		boolean aux = false;
-		System.out.println("--==| Jogo de pedra, papel e tesoura |==--\nAqui vocês definirão a ordem de jogadas, vulgo quem começa\n[1] - Pedra\n[2] - Papel\n[3] - Tesoura");
-		while(aux == false) {
-			System.out.println(p1.name + ", Escolha: ");
-			e1 = input.nextInt(); 
-			System.out.println(p2.name + ", Escolha: ");
-			e2 = input.nextInt();
-			if(e1 == e2) {
-				System.out.println("Empate, tentem novamente!");
-			} else {
-				aux = true;
-				switch(e1) {
-					case 1:
-						switch(e2) {
-							case 2:
-								System.out.println(p1.name + ", Parabéns, você perdeu!");
-								break;
-							case 3:
-								System.out.println(p2.name + ", Parabéns, você perdeu!");
-								break;
-						}
-					case 2:
-						switch(e2) {
-							case 1:
-								System.out.println(p2.name + ", Parabéns, você perdeu!");
-								break;
-							case 3:
-								System.out.println(p1.name + ", Parabéns, você perdeu!");
-								break;
-						}
-					case 3:
-						switch(e2) {
-							case 1:
-								System.out.println(p1.name + ", Parabéns, você perdeu!");
-								break;
-							case 2:
-								System.out.println(p2.name + ", Parabéns, você perdeu!");
-								break;
-						}
-					default:
-						System.out.println("||| ERRO INESPERADO |||");
-				}
-			}
-		}
-	}
+	
 	
 	public void changePlayerPos (Player p1, Player p2) {
 		Player aux;
