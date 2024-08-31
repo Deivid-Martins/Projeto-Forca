3c3e3b0406ade0ecd1a2ad9d024a6d942694262f
Deivid Martins <159496589+Deivid-Martins@users.noreply.github.com>
2024-08-31 14:16:07 -0300

    Iniciando função para definir quem começa

--- src/domain/Forca.java
+++ src/domain/Forca.java
@@ -1,10 +1,13 @@
 // Classe que gerencia o jogo da forca em si
 package domain;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Forca {
+	private Random random = new Random();
 	private Scanner input = new Scanner(System.in);
+	
 	public String word; // Palavra escolhida
 	public Player[] players; // Array de jogadores indefinido
 	
@@ -19,9 +22,13 @@ public class Forca {
 			players[i] = new Player(); // Cria um novo jogador para cada posição do array
 		}
 	}
-	
 
 	public String getWord() {
 		return word;
+	}
+	
+	// Função para definir quem deve começar
+	public void inicialPlayer () {
+		
 	}
 }
