427245632673af93a7d388486888c0498818a62a
Kayke Nícolas <148629652+KaykeNicolas007@users.noreply.github.com>
2024-09-13 18:55:18 -0300

    Agora vai em

--- src/domain/Forca.java
+++ src/domain/Forca.java
@@ -11,7 +11,7 @@ public class Forca {
 	public Player player; // Array de jogadores indefinido
 	public Dicionario dict; // Dicionário de palavras
 	public Tool tool; // Ferramentas
-	
+
 	
 	// Construtor da forca com jogadores e palavra definidos
 	public Forca () {
