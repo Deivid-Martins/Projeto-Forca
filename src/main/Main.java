50221bb5dd974241bce278cedcc3ffdccf305f3a
Kayke Nícolas <148629652+KaykeNicolas007@users.noreply.github.com>
2024-09-15 11:57:36 -0300

    Limpeza da classe Forca
    Funções muito grandes foram quebradas em partes menores. Dentro da Forca, todas os métodos estão comentados.

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,32 +2,14 @@
 // Classe responsável por iniciar o jogo da forca
 package main;
 
-import java.util.Scanner;
 import domain.Forca; // A própria forca
 
 public class Main {
 	public static void main(String[] args) {
-		// Mensagens de imersão
-		System.out.println("-----===== Olá, bem vindo ao jogo da forca!!! =====-----\n\n");
-
-		Scanner input = new Scanner(System.in);
-		char opc;
+		Forca forca = new Forca(); // Forca
+		forca.definirPalavra();
 		
-		do {
-			Forca forca = new Forca(); // Forca
-			
-			forca.chutarLetra();
-
-			do
-			{
-				System.out.print("Você deseja continuar? [s/n]: ");
-				opc = input.next().charAt(0);
-			} while(opc != 's' && opc != 'n');
-			
-		}while(opc != 'n');
-		
+		forca.jogar();
 		System.out.println("Obrigado por usar nosso programa!");
-		
-		input.close();
 	}
 }
